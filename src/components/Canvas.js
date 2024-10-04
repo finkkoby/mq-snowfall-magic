277a702bad05965def61e4be1f908e804878c7a7
Koby Fink <finkkoby@icloud.com>
2024-10-04 12:29:14 -0400

    create canvas component

---
+++ src/components/Canvas.js
@@ -0,0 +1,13 @@
+
+
+function Canvas() {
+    return (
+        <div>
+            <p>empty canvas</p>
+            <canvas>
+            </canvas>
+        </div>
+    )
+}
+
+export default Canvas
