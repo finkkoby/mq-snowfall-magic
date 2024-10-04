51b2c00d3b54bec95344eda9288fa022394ed851
Koby Fink <finkkoby@icloud.com>
2024-10-04 16:32:31 -0400

    restructure existing code, dry up code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-
 import Header from './components/Header';
-import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
-      <Canvas />
       <Header />
     </div>
   );
